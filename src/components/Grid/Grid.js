75320c22c4937ba9278f47b5d3870c4b9263c93a
gingus55 <gingus55@hotmail.com>
2023-08-31 21:25:56 +0100

    initial code added

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./index.css";
+
+function Grid() {
+  return (
+    <div id="holder">
+      <table>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default Grid;
